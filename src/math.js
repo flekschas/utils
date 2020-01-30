4b7c3280d329ec05c0e952dee5abda4e64ba945f
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-01-30 18:45:35 -0500

    Rename for clarity

--- src/math.js
+++ src/math.js
@@ -9,6 +9,15 @@
 export const max = (a, b) => (a > b ? a : b);
 
 /**
+ * Non-negative modulo function. E.g., `mod(-1, 5) === 4` while `-1 % 5 === -1`.
+ *
+ * @param {number} x - Dividend
+ * @param {number} y - Divisor
+ * @return {number} Remainder
+ */
+export const mod = (x, y) => ((x % y) + x) % y;
+
+/**
  * Fast version of `Math.min`. Based on
  *   https://jsperf.com/math-min-max-vs-ternary-vs-if/24 `Math.max` is not
  *   very fast
