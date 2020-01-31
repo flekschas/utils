7ad4175451dd866361e45ce41e5104ac4b1f5d7b
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-01-30 20:33:41 -0500

    Refactor and document

--- src/math.js
+++ src/math.js
@@ -1,12 +1,38 @@
 /**
- * Fast version of `Math.max`. Based on
- *   https://jsperf.com/math-min-max-vs-ternary-vs-if/24 `Math.max` is not
- *   very fast
- * @param   {number}  a  Value A
- * @param   {number}  b  Value B
- * @return  {boolean}  If `true` A is greater than B.
+ * Identity function
+ * @param   {*}  x  Any kind of value
+ * @return  {*}  `x`
  */
-export const max = (a, b) => (a > b ? a : b);
+export const identity = x => x;
+
+/**
+ * Get the maximum number of a vector
+ *
+ * @description
+ * This version is muuuch faster than `Math.max(...v)`.
+ *
+ * @param {array} v - Numerical vector
+ * @return {number} The largest number
+ */
+export const max = v => v.reduce((_max, a) => (_max > a ? _max : a), -Infinity);
+/**
+ * Get the mean of a vector
+ *
+ * @param {array} v - Numerical vector
+ * @return {number} The mean
+ */
+export const mean = v => sum(v) / v.length;
+
+/**
+ * Get the minimum number of a vector
+ *
+ * @description
+ * This version is muuuch faster than `Math.min(...v)`.
+ *
+ * @param {array} v - Numerical vector
+ * @return {number} The smallest number
+ */
+export const min = v => v.reduce((_min, a) => (_min < a ? _min : a), Infinity);
 
 /**
  * Non-negative modulo function. E.g., `mod(-1, 5) === 4` while `-1 % 5 === -1`.
@@ -18,19 +44,9 @@ export const max = (a, b) => (a > b ? a : b);
 export const mod = (x, y) => ((x % y) + x) % y;
 
 /**
- * Fast version of `Math.min`. Based on
- *   https://jsperf.com/math-min-max-vs-ternary-vs-if/24 `Math.max` is not
- *   very fast
- * @param   {number}  a  Value A
- * @param   {number}  b  Value B
- * @return  {boolean}  If `true` A is smaller than B.
+ * Get the sum of a vector
+ *
+ * @param {array} v - Numerical vector
+ * @return {number} The sum
  */
-export const min = (a, b) => (a < b ? a : b);
-
-export const sumAggregator = values => values.reduce((s, v) => s + v, 0);
-
-export const meanAggregator = values => sumAggregator(values) / values.length;
-
-export const maxAggregator = array => array.reduce(max, -Infinity);
-
-export const minAggregator = array => array.reduce(min, Infinity);
+export const sum = values => values.reduce((s, v) => s + v, 0);
