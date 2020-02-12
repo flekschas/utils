96f94d68a9ddb366b8a634de47c33ab3b50049e0
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-11 19:47:31 -0500

    Expand color conversion-related utils

--- src/color.js
+++ src/color.js
@@ -1,23 +1,110 @@
-export const isHexStr = str => str[0] === '#';
+import {
+  isHex,
+  isNormFloatArray,
+  isRgbArray,
+  isRgbaArray
+} from './type-checking';
+import { normalize as norm } from './vector';
 
-export const isRgbStr = str => str.substring(0, 4) === 'rgb(';
+/**
+ * Convert a HEX string to its decimal representation
+ * @param {string} hex - HEX string
+ * @return {number} Decimal representation
+ */
+export const hexToDec = hex => parseInt(hex.substr(1), 16);
 
-export const isRgbaStr = str => str.substring(0, 4) === 'rgba';
+/**
+ * Convert a HEX-encoded color to an RGB-encoded color
+ * @param {string} hex - HEX-encoded color string.
+ * @param {boolean} normalize - If `true` the returned RGB values will be
+ *   normalized to `[0,1]`.
+ * @return {array} Triple holding the RGB values.
+ */
+export const hexToRgbArray = (hex, normalize = false) =>
+  hex
+    .replace(
+      /^#?([a-f\d])([a-f\d])([a-f\d])$/i,
+      (m, r, g, b) => `#${r}${r}${g}${g}${b}${b}`
+    )
+    .substring(1)
+    .match(/.{2}/g)
+    .map(x => parseInt(x, 16) / 255 ** normalize);
 
-export const hexStrToDec = hexStr => parseInt(hexStr.substr(1), 16);
+/**
+ * Convert a HEX-encoded color to an RGBA-encoded color
+ * @param {string} hex - HEX-encoded color string.
+ * @param {boolean} normalize - If `true` the returned RGBA values will be
+ *   normalized to `[0,1]`.
+ * @return {array} Triple holding the RGBA values.
+ */
+export const hexToRgbaArray = (hex, normalize = false) => [
+  ...hexToRgbArray(hex, normalize),
+  255 ** !normalize
+];
 
-export const rgbStrToDec = hexStr =>
-  hexStr
-    .match(/(\d+),\s*(\d+),\s*(\d+),?\s*([\d.]+)?/)
-    .slice(1, 4)
+/**
+ * Convert RGB(A) string to its array representation
+ * @param {string} rgbStr - RGB(A) string
+ * @return {number} RGB(A) array
+ */
+export const rgbStrToRgbArray = rgbStr =>
+  rgbStr
+    .match(/[\d.]+/g)
+    .slice(0, 4)
+    .map(x => +x);
+
+/**
+ * Same as `rgbStrToRgbArray()`
+ */
+export const rgbaStrToRgbaArray = rgbStrToRgbArray;
+
+/**
+ * Convert RGB string to its decimal representation
+ * @param {string} rgbStr - RGB string
+ * @return {number} Decimal representation
+ */
+export const rgbStrToDec = rgbStr =>
+  rgbStrToRgbArray(rgbStr)
+    .slice(0, 3)
     // eslint-disable-next-line no-bitwise
     .map((x, i) => +x << (8 * (2 - i)))
     .reduce((x, sum) => sum + x, 0);
 
+/**
+ * Convert RGB values to a HEX string
+ * @param {number} r - Red component
+ * @param {number} g - Green component
+ * @param {number} b - Blue component
+ * @return {string} HEX string
+ */
 export const rgbToHex = (r, g, b) => {
   const componentToHex = c => {
     const hex = c.toString(16);
     return hex.length === 1 ? `0${hex}` : hex;
   };
   return `#${componentToHex(r)}${componentToHex(g)}${componentToHex(b)}`;
+};
+
+/**
+ * Convert a color to an RGBA color
+ * @param {*} color - Color to be converted. Currently supports:
+ *   HEX, RGB, or RGBA.
+ * @param {boolean} normalize - If `true` the returned RGBA values will be
+ *   normalized to `[0,1]`.
+ * @return{array} Quadruple defining an RGBA color.
+ */
+export const toRgbaArray = (color, normalize) => {
+  if (isRgbaArray(color))
+    return normalize && !isNormFloatArray(color) ? normalize(color) : color;
+
+  if (isRgbArray(color))
+    return [...(normalize ? norm(color) : color), 255 ** !normalize];
+
+  if (isHex(color)) return hexToRgbaArray(color, normalize);
+
+  console.warn(
+    'Only HEX, RGB, and RGBA are handled by this function. Returning white instead.'
+  );
+
+  return normalize ? [1, 1, 1, 1] : [255, 255, 255, 255];
 };
--- src/type-checking.js
+++ src/type-checking.js
@@ -5,3 +5,79 @@
  */
 export const isFunction = obj =>
   !!(obj && obj.constructor && obj.call && obj.apply);
+
+/**
+ * Tests if a string is a valid HEX color encoding
+ * @param {string} hex - HEX-encoded color string.
+ * @return {boolean} If `true` the string is a valid HEX color encoding.
+ */
+export const isHex = hex => /(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(hex);
+
+/**
+ * Tests if a number is in `[0,1]`.
+ * @param {number} x - Number to be tested.
+ * @return {boolean} If `true` the number is in `[0,1]`.
+ */
+export const isNormFloat = x => x >= 0 && x <= 1;
+
+/**
+ * Tests if an array consist of normalized numbers that are in `[0,1]` only.
+ * @param {array} a - Array to be tested
+ * @return {boolean} If `true` the array contains only numbers in `[0,1]`.
+ */
+export const isNormFloatArray = a => Array.isArray(a) && a.every(isNormFloat);
+
+/**
+ * Tests if an array is encoding an RGB color.
+ * @param {array} rgb - Array to be tested
+ * @return {boolean} If `true` the array hold a triple of Uint8 numbers or
+ *   a triple of normalized floats.
+ */
+export const isRgbArray = rgb =>
+  rgb.length === 3 && (isNormFloatArray(rgb) || isUint8Array(rgb));
+
+/**
+ * Tests if an array is encoding an RGBA color.
+ * @param   {array}  rgb  Array to be tested
+ * @return  {boolean}  If `true` the array hold a quadruple of Uint8 numbers or
+ *   a quadruple of normalized floats.
+ */
+export const isRgbaArray = rgba =>
+  rgba.length === 4 && (isNormFloatArray(rgba) || isUint8Array(rgba));
+
+/**
+ * Tests if a string is encoding an RGB color.
+ * @param {string} rgb - String to be tested
+ * @return {boolean} If `true` the array hold a triple of Uint8 numbers or
+ *   a triple of normalized floats.
+ */
+export const isRgbStr = str => str.substring(0, 4) === 'rgb(';
+
+/**
+ * Tests if a string is encoding an RGBA color.
+ * @param {string} rgb - String to be tested
+ * @return {boolean} If `true` the array hold a quadruple of Uint8 numbers or
+ *   a quadruple of normalized floats.
+ */
+export const isRgbaStr = str => str.substring(0, 4) === 'rgba';
+
+/**
+ * Tests if a variable is a string
+ * @param {*} s - Variable to be tested
+ * @return {boolean} If `true` variable is a string
+ */
+export const isString = s => typeof s === 'string' || s instanceof String;
+
+/**
+ * Tests if a number is an interger and in `[0,255]`.
+ * @param {number} x - Number to be tested.
+ * @return {boolean} If `true` the number is an interger and in `[0,255]`.
+ */
+export const isUint8 = x => Number.isInteger(x) && x >= 0 && x <= 255;
+
+/**
+ * Tests if an array consist of Uint8 numbers only.
+ * @param {array} a - Array to be tested.
+ * @return {boolean} If `true` the array contains only Uint8 numbers.
+ */
+export const isUint8Array = a => Array.isArray(a) && a.every(isUint8);
