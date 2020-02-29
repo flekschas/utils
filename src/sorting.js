b2be51cfbaecfa99fcdbe210bc1132c03d6522a4
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-02-29 13:19:16 -0500

    Allow sorting objects with sortPos()

--- src/sorting.js
+++ src/sorting.js
@@ -35,12 +35,17 @@ export const argSort = (
     }, []);
 
 /**
- * Return the sort position of each element in a list
+ * Return the sort position of each element in an array or object
  *
  * @example
- *   X = [9, 5, 11, -1, 0]
- *   idxPos = sortPos(X)
+ *   let array = [9, 5, 11, -1, 0];
+ *   let pos = sortPos(array)
  *   >> [3, 2, 4, 0, 1]
+ *
+ *   let object = { 1: 9, 2: 5, 11: 11, 100: -1, 999: 0 };
+ *   let pos = sortPos(object)
+ *   >> {1: 9, 2: 5, 11: 11, 100: -1, 999: 0}
+ *
  *   I.e., the first element of X is at position idxPos[0] == 3
  *
  * @param {array} array - Array of numerical values
@@ -48,18 +53,18 @@ export const argSort = (
  * @return {array} Array of the sorted value positions
  */
 export const sortPos = (
-  array,
+  source,
   { getter = identity, comparator = sortAsc, ignoreNull = false } = {}
 ) =>
-  array
+  Object.entries(source)
     .map(
       ignoreNull
-        ? (x, i) => (getter(x) === null ? undefined : [getter(x), i])
-        : (x, i) => [getter(x), i]
+        ? ([id, x]) => (getter(x) === null ? undefined : [id, getter(x)])
+        : ([id, x]) => [id, getter(x)]
     )
-    .sort((a, b) => comparator(a[0], b[0]))
+    .sort((a, b) => comparator(a[1], b[1]))
     .reduce((out, tuple, i) => {
       if (!tuple) return out;
-      out[tuple[1]] = i;
+      out[tuple[0]] = i;
       return out;
-    }, []);
+    }, new source.constructor());
