9450d2b23d8ffdf4c977f1413480ad91a8492d6a
Fritz Lekschas <932103+flekschas@users.noreply.github.com>
2020-08-25 12:04:18 -0400

    Revert accidental change

--- rollup.test.config.js
+++ rollup.test.config.js
@@ -4,18 +4,18 @@ import resolve from 'rollup-plugin-node-resolve';
 
 export default {
   input: [
-    // './tests/animation.js',
-    // './tests/array.js',
-    // './tests/color.js',
-    // './tests/dom.js',
-    // './tests/functional-programming.js',
-    // './tests/geometry.js',
-    // './tests/math.js',
-    // './tests/object.js',
-    // './tests/sorting.js',
-    // './tests/string.js',
-    // './tests/timing.js',
-    // './tests/type-checking.js',
+    './tests/animation.js',
+    './tests/array.js',
+    './tests/color.js',
+    './tests/dom.js',
+    './tests/functional-programming.js',
+    './tests/geometry.js',
+    './tests/math.js',
+    './tests/object.js',
+    './tests/sorting.js',
+    './tests/string.js',
+    './tests/timing.js',
+    './tests/type-checking.js',
     './tests/vector.js'
   ],
   output: {
